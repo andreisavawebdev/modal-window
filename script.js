cafdc15e99214abed8730347945fb7c7fae480a9
Andrei Sava <andreisava88@gmail.com>
2021-09-16 19:01:27 +0300

    Finished HTML and CSS

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+let btn1 = document.getElementById('modal-1');
+let btn2 = document.getElementById('modal-2');
+let btn3 = document.getElementById('modal-3');
+
+let modalWindow1 = document.querySelector('.modal-1');
+let modalWindow2 = document.querySelector('.modal-2');
+let modalWindow3 = document.querySelector('.modal-3');
